a7d1bd7adb035f36d41eea123383081db8ad2335
anthony ang <anthonyang25@gmail.com>
2017-11-03 22:14:41 +0800

    Added new countries to the CountriesActivity class

--- otherLangApp_v0.0.0/app/src/main/java/com/example/android/otherLang/CountriesActivity.java
+++ otherLangApp_v0.0.0/app/src/main/java/com/example/android/otherLang/CountriesActivity.java
@@ -23,16 +23,20 @@ public class CountriesActivity extends AppCompatActivity {
         words.add(new Word("denmark", "Dān mài\n" + "丹麦", R.drawable.denmark));
         words.add(new Word("france", "Fà guó\n" + "法国", R.drawable.france));
         words.add(new Word("germany", "Dé guó\n" + "德国", R.drawable.germany));
+        words.add(new Word("hong kong", "Xiāng gǎng\n" + "香港", R.drawable.hong_kong));
         words.add(new Word("italy", "Yì dà lì\n" + "意大利", R.drawable.italy));
         words.add(new Word("japan", "Rì běn\n" + "日本", R.drawable.japan));
         words.add(new Word("malaysia", "Mǎ lái xī yà\n" + "马来西亚", R.drawable.malaysia));
-        words.add(new Word("norway", "Bái sè\n" + "挪威", R.drawable.norway));
+        words.add(new Word("norway", "Nuó wēi\n" + "挪威", R.drawable.norway));
+        words.add(new Word("philippines", "Fēi lǜ bīn\n" + "菲律宾", R.drawable.philippines));
         words.add(new Word("russia", "É guó\n" + "俄国", R.drawable.russia));
         words.add(new Word("singapore", "Xīn jiā pō\n" + "新加坡", R.drawable.singapore));
         words.add(new Word("south korea", "Hán guó\n" + "韩国", R.drawable.south_korea));
         words.add(new Word("sweden", "Ruì diǎn\n" + "瑞典", R.drawable.sweden));
+        words.add(new Word("taiwan", "Táiwān\n" + "台湾", R.drawable.taiwan));
         words.add(new Word("united kingdom", "Yīng guó\n" + "英国", R.drawable.united_kingdom));
         words.add(new Word("united states", "Měi guó\n" + "美国", R.drawable.united_states));
+        words.add(new Word("vietnam", "Yuè nán\n" + "越南", R.drawable.vietnam));
 
         WordAdapter adapter = new WordAdapter(this, words,R.color.category_countries); // list of data + instruction to make list item view
         ListView listView = (ListView) findViewById(R.id.list); //reference listView in xml, listView -> 'empty container'
@@ -41,6 +45,10 @@ public class CountriesActivity extends AppCompatActivity {
 //        gridview.setAdapter(adapter);
     }
 }
+
+
+
+
 
 
 
